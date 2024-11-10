2aa12295cc34f792716985d6853526cb6248b44c
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-10 11:46:51 +0100

    Remove api.js from repository

--- src/api.js
+++
@@ -1,5 +0,0 @@
-const KEY = '24c27bc23bb1a89586e7dd1b';
-const urlCurrencyConvertions= `https://v6.exchangerate-api.com/v6/${KEY}/pair/`;
-const urlCurrencies= 'https://openexchangerates.org/api/currencies.json'; 
-
-export {urlCurrencies, urlCurrencyConvertions};
