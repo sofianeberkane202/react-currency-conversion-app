d746a1a3fff85524c4adc7c1caa03368cded9262
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-10 11:11:13 +0100

    finish the initial building of a app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import { useEffect, useState } from "react";
 import { urlCurrencies,urlCurrencyConvertions } from "./api";
 import { fetchData } from "./helper";
 
+const keyWords={
+    TARGET: 'target',
+    BASED:'based',
+}
+
 export default function App(){
 
     const [currencies,setCurrencies] = useState(new Map());
-    const [basedConversion, setBasedConversion] = useState('noSelection');
-    const [targetCondersion, setTargetConversion] = useState('noSelection');
+    const [basedConversion, setBasedConversion] = useState('no');
+    const [targetConversion, setTargetConversion] = useState('no');
+
+    const [conversionRate, setConversionRate] = useState(null);
+
+    const [inputBased, setInputBased]= useState(1);
+    const [inputTarget, setInputTarget]= useState(null);
+
+    
 
     
     useEffect(function(){
@@ -25,14 +37,70 @@ export default function App(){
         fetchCurrencies();
     },[]);
 
+    useEffect(function(){
+
+        async function fetchConversionData(){
+            try {
+                const url = urlCurrencyConvertions + `${basedConversion}/${targetConversion}`;
+                const data = await fetchData(url);
+                console.log(data);
+                setConversionRate(data.conversion_rate); 
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if(basedConversion === 'no' || targetConversion === 'no') {
+            setConversionRate(null);
+            return;
+        }
+
+        fetchConversionData();
+
+    },[basedConversion,targetConversion])
+
+    useEffect(function(){
+        setInputTarget(conversionRate * inputBased);
+    },[conversionRate]);
+
+    function handleConversion(value, type){
+        
+        if(type === keyWords.BASED){
+            setInputTarget(Number(conversionRate * value).toFixed(3));
+            setInputBased(value);
+        }
+
+        if(type === keyWords.TARGET){
+            setInputBased(Number(value  /conversionRate).toFixed(3));
+            setInputTarget(value);
+        }
+    }
+
   return (
     <div className="app">
         <div className="box">
-            <input type="number" value={1}/>
+            { conversionRate ?
+            <input 
+            type="number" 
+            min={1}
+            value={inputBased}
+            onChange={(e) => handleConversion(+e.target.value,keyWords.BASED)} 
+            />
+
+            :
+
+            <input 
+            type="number"
+            min={1} 
+            value={inputBased}
+            readOnly 
+            onChange={(e) => handleConversion(+e.target.value,keyWords.BASED)}
+            />
+            }
 
 
             <select value={basedConversion} onChange={(e) => setBasedConversion(e.target.value)}>
-                <option value={'noSelection'}>Select Currency</option>
+                <option value={'no'}>Select Currency</option>
                 {
                     Array.from(currencies.entries()).map(([code,country]) => 
                     <option key={code} value={code}>{country}</option>)
@@ -41,17 +109,20 @@ export default function App(){
         </div>
 
         <div className="box">
-            <input type="number"/>
-            <select value={targetCondersion} onChange={(e) => setTargetConversion(e.target.value)}>
-            <option value={'noSelection'}>Select Currency</option>
+            <input 
+            type="number" 
+            value={inputTarget} 
+            onChange={(e) => handleConversion(+e.target.value,keyWords.TARGET)}
+            />
+
+            <select value={targetConversion} onChange={(e) => setTargetConversion(e.target.value)}>
+            <option value={'no'}>Select Currency</option>
                 {
                     Array.from(currencies.entries()).map(([code,country]) => 
                     <option key={code} value={code}>{country}</option>)
                 }
             </select>
         </div>
-
-        <p className="result">0</p>
     </div>
 
   )
